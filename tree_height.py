fe3675839fd98c00870a7f283fe35200d5e80db4
Nikita <124347632+Endizzy@users.noreply.github.com>
2023-03-06 09:02:07 +0200

    okey, i think its fine

--- tree_height.py
+++ tree_height.py
@@ -2,86 +2,42 @@
 import sys
 import threading
 
-
-
-
-
-
-
-def compute_height(n , nodes):
+def compute_height(n,nodes):
     class Node:
-        def __init__(self, key) -> None:
+        def __init__(self, key):
             self.key = key
             self.lst = []
+    map = [Node(x) for x in range(n)]
+    i = 0
+    for i in range(n):
+        if nodes[i] == -1: 
+            root = map[i]
+        else:
+            map[nodes[i]].lst.append(map[i])
 
-        def __repr__(self) -> str:
-            return f"key: {self.key} , lst: {self.lst}"
+    def calc(node):
+        height = 0
+        if not node.lst:
+            return 1
+        for child in node.lst:
+            height =  max(height, calc(child))
+        return height + 1
 
-
-    class Tree:
-        root = Node
-
-        def __init__(self, n):
-            self.n = n
-            self.map = [Node(x) for x in range(n)]
-
-        def build_tree(self,childrens, i = 0):
-            if childrens[i] == -1: 
-                self.root = self.map[i]
-
-            else:
-                self.map[childrens[i]].lst.append(self.map[i])
-            
-            if i < self.n -1:
-                self.build_tree(childrens, i+1)
-
-        def get_height(self):
-            return self.__get_height(self.root)
-
-        def __get_height(self, node):
-            height = 0
-            if not node.lst:
-                return 1
-
-            for child in node.lst:
-                height =  max(height, self.__get_height(child))
-            return height+1
-
-
-    
-    tree = Tree(n)
-    tree.build_tree(nodes)
-    return tree.get_height()
-
+    return calc(root)
 
 def main():
-    def getTest(filename):
-        path = "./test/"
-        with open(f"{path + filename}")  as test:
-            n = int(test.readline())
-            nodes = list(map(int, test.readline().split()))
-
-        return n , nodes
-
-
-
     command = input()
-    if command == "I":
+    if "I" in command:
         n = int(input("input count: "))
         nodes = list(map(int, input().split()))
-        print(compute_height(n,nodes))
 
-
-    if command == "F":
+    elif "F" in command:
         filename = input()
-        test = getTest(filename)
-        n = test[0]
-        nodes = test[1]
-        print(compute_height(n,nodes))
+        with open("test/" + filename, 'r')  as test:
+            n = int(test.readline())
+            nodes = list(map(int, test.readline().split()))
+    print(compute_height(n,nodes))
 
-
-
-        
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
 threading.Thread(target=main).start()
