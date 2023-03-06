e5d73cd571cbe924f4778d02b290e07e05639327
Nikita <124347632+Endizzy@users.noreply.github.com>
2023-03-06 08:59:43 +0200

    ??

--- tree_height.py
+++ tree_height.py
@@ -1,65 +1,70 @@
 # Nikita Smirnovs 221RDB433
 import sys
 import threading
-import os
-
-
-class Node:
-    def __init__(self, key) -> None:
-        self.key = key
-        self.lst = []
-
-    def __repr__(self) -> str:
-        return f"key: {self.key} , lst: {self.lst}"
-
-
-class Tree:
-    root = Node
-
-    def __init__(self, n):
-        self.n = n
-        self.map = [Node(x) for x in range(n)]
-
-    def build_tree(self,childrens, i = 0):
-        if childrens[i] == -1: 
-            self.root = self.map[i]
-
-        else:
-            self.map[childrens[i]].lst.append(self.map[i])
-        
-        if i < self.n -1:
-            self.build_tree(childrens, i+1)
-
-    def get_height(self):
-        return self.__get_height(self.root)
-
-    def __get_height(self, node):
-        height = 0
-        if not node.lst:
-            return 1
-
-        for child in node.lst:
-            height =  max(height, self.__get_height(child))
-        return height+1
 
 
 
-def getTest(filename):
-    path = "./test/"
-    with open(f"{path + filename}")  as test:
-        n = int(test.readline())
-        nodes = list(map(int, test.readline().split()))
 
-    return n , nodes
+
 
 
 def compute_height(n , nodes):
+    class Node:
+        def __init__(self, key) -> None:
+            self.key = key
+            self.lst = []
+
+        def __repr__(self) -> str:
+            return f"key: {self.key} , lst: {self.lst}"
+
+
+    class Tree:
+        root = Node
+
+        def __init__(self, n):
+            self.n = n
+            self.map = [Node(x) for x in range(n)]
+
+        def build_tree(self,childrens, i = 0):
+            if childrens[i] == -1: 
+                self.root = self.map[i]
+
+            else:
+                self.map[childrens[i]].lst.append(self.map[i])
+            
+            if i < self.n -1:
+                self.build_tree(childrens, i+1)
+
+        def get_height(self):
+            return self.__get_height(self.root)
+
+        def __get_height(self, node):
+            height = 0
+            if not node.lst:
+                return 1
+
+            for child in node.lst:
+                height =  max(height, self.__get_height(child))
+            return height+1
+
+
+    
     tree = Tree(n)
     tree.build_tree(nodes)
     return tree.get_height()
 
 
 def main():
+    def getTest(filename):
+        path = "./test/"
+        with open(f"{path + filename}")  as test:
+            n = int(test.readline())
+            nodes = list(map(int, test.readline().split()))
+
+        return n , nodes
+
+
+
     command = input()
     if command == "I":
         n = int(input("input count: "))
